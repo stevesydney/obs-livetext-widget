a7800f8cc29feae3b630938363ee8bb89b99740c
Steve Farrugia <steve@steves-macbook-pro-2.home>
2022-11-18 15:52:15 +0100

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/live-text', (req, res) => {
+    res.sendFile(__dirname + '/live-text.html');
+});
+
+app.get('/dashboard', (req, res) => {
+    res.sendFile(__dirname + '/dashboard.html');
+});
+
+let currentLiveText;
+
+io.on('connection', (socket) => {
+    if (currentLiveText) {
+        io.emit('live text', currentLiveText);
+    }
+    socket.on('live text', (liveText) => {
+        io.emit('live text', liveText);
+        currentLiveText = liveText;
+    });
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
